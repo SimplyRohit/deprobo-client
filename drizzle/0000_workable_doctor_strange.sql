db273d94607cf0122991232120bc549974d20bda
SimplyRohit <rohitjaatjaat073@gmail.com>
2025-07-19 10:59:53 +0530

    inggest imp

--- drizzle/0000_workable_doctor_strange.sql
+++ drizzle/0000_workable_doctor_strange.sql
@@ -1,28 +1,25 @@
 CREATE TABLE "bets" (
 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "bets_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
-	"authority" varchar(44) NOT NULL,
-	"market" varchar(44) NOT NULL,
+	"authority" varchar(50) NOT NULL,
+	"marketid" varchar(50) NOT NULL,
 	"amount" integer NOT NULL,
-	"outcome" integer NOT NULL,
-	"claimed" integer DEFAULT 0 NOT NULL
+	"outcome" boolean NOT NULL,
+	"claimed" boolean DEFAULT false NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE "markets" (
 	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "markets_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"marketid" varchar(50) NOT NULL,
 	"question" varchar(255) NOT NULL,
 	"category" varchar(50) NOT NULL,
 	"closeTime" integer NOT NULL,
 	"createdAt" integer NOT NULL,
-	"resolved" integer DEFAULT 0 NOT NULL,
-	"winningOutcome" integer DEFAULT 0 NOT NULL,
-	"yesPool" varchar(44) NOT NULL,
-	"noPool" varchar(44) NOT NULL,
+	"authority" varchar(44) NOT NULL,
+	"resolved" boolean DEFAULT false NOT NULL,
+	"winningOutcome" boolean DEFAULT false NOT NULL,
 	"totalYes" integer DEFAULT 0 NOT NULL,
 	"totalNo" integer DEFAULT 0 NOT NULL,
-	"yesUsers" integer DEFAULT 0 NOT NULL,
-	"noUsers" integer DEFAULT 0 NOT NULL,
-	"authority" varchar(44) NOT NULL,
-	"bet" integer DEFAULT 0 NOT NULL
+	CONSTRAINT "markets_marketid_unique" UNIQUE("marketid")
 );
 --> statement-breakpoint
-DROP TABLE "users" CASCADE;
+ALTER TABLE "bets" ADD CONSTRAINT "bets_marketid_markets_marketid_fk" FOREIGN KEY ("marketid") REFERENCES "public"."markets"("marketid") ON DELETE no action ON UPDATE no action;
